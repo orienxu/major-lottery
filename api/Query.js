cc36296286c22f98a41f669ed8fc8238c19a879b
didntpay <weifengli2014@gmail.com>
2020-09-03 20:18:37 -0700

    finished connecting to the sql server

--- api/Query.js
+++ api/Query.js
@@ -1,20 +1,29 @@
-const sql = require('mssql');
-
+const sql = require('mysql');
+const LOGIN_CHECK_USER_EXIST = "Select U.username From Users As U Where U.username = ?";
+const LOGIN_CHECK_CRED = "Select U.username From Users As U Where U.username = ? and U.pass = ?";
 class Query {
     constructor(config) {
         this.config = config;
-        this.pool = null;
+        this.connection = null;
     }
 
-    init() {
-        this.pool = new sql.ConnectionPool(this.config);
-        this.pool.connect(function (err) {
-            if (err) console.log(err);            
+    async init() {
+        this.connection = sql.createConnection(this.config);
+        this.connection.connect(function (err) {
+            if(err) console.error("error" + err.stack);
+            console.log("We made it");
         });
+
+        this.logIn("weifeng1", "mf");
     }
 
     logIn(username, passwd){
-
+        this.connection.query(LOGIN_CHECK_USER_EXIST, [username], function (err, results, fields) {
+            if (err) {
+                throw err;
+            }
+            console.log(results[0]);
+        });
     }
 
     register(username, passwd){
@@ -25,8 +34,17 @@ class Query {
         
     }
 
+    // errorHandler(err) {
+    //      if (err) {
+    //          console.error("error" + err.stack);
+    //          throw err;
+    //      }
+    // }
+
     exit() {
-        this.pool.close();
+        this.connection.end(function(err) {
+            console.log("Logging out of the db");
+        });
     }
 }
 
