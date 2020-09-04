b31e073291cd5dac886bac257f381edcb2344edc
didntpay <weifengli2014@gmail.com>
2020-09-04 09:27:08 -0700

    finished login with no transaction

--- api/Query.js
+++ api/Query.js
@@ -1,6 +1,9 @@
 const sql = require('mysql');
-const LOGIN_CHECK_USER_EXIST = "Select U.username From Users As U Where U.username = ?";
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
+const LOGIN_CHECK_USER_EXIST = "Select U.username, U.salt From Users As U Where U.username = ?";
 const LOGIN_CHECK_CRED = "Select U.username From Users As U Where U.username = ? and U.pass = ?";
+
 class Query {
     constructor(config) {
         this.config = config;
@@ -11,18 +14,35 @@ class Query {
         this.connection = sql.createConnection(this.config);
         this.connection.connect(function (err) {
             if(err) console.error("error" + err.stack);
-            console.log("We made it");
         });
 
-        this.logIn("weifeng1", "mf");
+        this.logIn("weifeng1", "123");
     }
 
     logIn(username, passwd){
-        this.connection.query(LOGIN_CHECK_USER_EXIST, [username], function (err, results, fields) {
+        var self = this;
+        var salt = null;
+        self.connection.query(LOGIN_CHECK_USER_EXIST, [username], function (err, results, fields) {            
             if (err) {
                 throw err;
             }
-            console.log(results[0]);
+            if (results.length < 0) {
+                console.error("Cannot find user");
+                return null;
+            }       
+            salt = results[0]['salt'];
+
+        });
+
+        var hashedPass = hash.update(passwd, salt).digest("hex");
+
+        //add the passd + salt encryption and hash to compare.
+        self.connection.query(LOGIN_CHECK_CRED, [username, hashedPass], function (err, results, fields) {
+            if (err) {
+                throw err;
+            }
+            return results[0]['username'];
+
         });
     }
 
