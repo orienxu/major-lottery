72ca37fdfdf4882f7b88ef519de74545de842f1b
alisa_yuxiao@qq.com <shiyx216@uw.edu>
2020-09-12 12:10:01 -0700

    add server action: ownedCards

--- api/Query.js
+++ api/Query.js
@@ -1,13 +1,13 @@
 const sql = require('mysql');
 const crypto = require('crypto');
+const userConfig = require('./Config');
+const { CARDS } = require('./Config');
 
 const LOGIN_CHECK_USER_EXIST = "Select U.username, U.salt From Users As U Where U.username = ?";
 const LOGIN_CHECK_CRED = "Select U.username From Users As U Where U.username = ? and U.pass = ?";
 const REGISTER_CHECK_USER_EXIST = "Select U.username From Users As U Where U.username = ? ";
 const REGISTER_ADD_USER = "INSERT INTO Users VALUES(?, ?, ?, ?)";
-
-const USER_NOT_FOUND = -1;
-const INCORRECT_PASSWORD_OR_USERNAME = -2;
+const VIEW_OWNED = "SELECT UC.cse, UC.ee FROM UserCard As UC JOIN Users As U ON UC.username = U.username WHERE U.username = ?";
 
 class Query {
     constructor(config) {
@@ -32,9 +32,10 @@ class Query {
             if(err) console.error("error" + err.stack);
         });
 
-        //console.log(this.logIn("weifeng", "123", this.logInHelper));
-        //this.register("weifeng", "123", this.registerHelper);
+        // console.log(this.logIn("weifeng", "123", this.logInHelper));
+        // this.register("weifeng", "123", this.registerHelper);
         // this.logIn("weifeng","123", this.registerHelper);
+        this.ownedCards("weifeng", this.ownedCardsHelper);
     }
 
     logIn(username, passwd, callback){
@@ -42,15 +43,15 @@ class Query {
         let salt = null;
         let res = null;
         const hash = crypto.createHash('sha256');
-        self.connection.query(LOGIN_CHECK_USER_EXIST, [username], function (err, results, fields) {            
+        self.connection.query(LOGIN_CHECK_USER_EXIST, [username], function (err, results, fields) {
             if (err) {
                 throw err;
             }
             if (results.length < 1) {
-                res = USER_NOT_FOUND;
+                res = userConfig.USER_NOT_FOUND;
                 callback(res);
                 return;
-            }       
+            }
             salt = results[0]['salt'];
             var hashedPass = hash.update(passwd, salt).digest("hex");
 
@@ -60,7 +61,7 @@ class Query {
                     throw err;
                 }
                 if (results.length == 0) {
-                    res = INCORRECT_PASSWORD_OR_USERNAME;
+                    res = userConfig.INCORRECT_PASSWORD_OR_USERNAME;
                 }
                 else {
                     res = results[0]['username'];
@@ -82,7 +83,7 @@ class Query {
             if (results.length == 0) {
                 var salt = self.generateSalt();
                 var hashedPass = hash.update(passwd, salt).digest("hex");
-    
+
                 self.connection.query(REGISTER_ADD_USER, [username, hashedPass, salt, 3], function (err, results, fields) {
                     if (err) {
                         throw err;
@@ -93,14 +94,14 @@ class Query {
                 callback("Username is already taken");
             }
         });
-        
+
     }
 
-    
 
-    updateUserCard(username) {
-        
-    }
+
+    // updateUserCard(username) {
+
+    // }
 
     // errorHandler(err) {
     //      if (err) {
@@ -108,6 +109,34 @@ class Query {
     //          throw err;
     //      }
     // }
+
+    ownedCards(username, callback) {
+        var self = this;
+        let res = null;
+        self.connection.query(VIEW_OWNED, [username], function (err, results) {
+          if (err) {
+              throw err;
+          }
+          if (results.length < 1) {
+              res = userConfig.EMPTY_OWNED;
+              callback(res); // ??
+              return;
+          }
+
+          for (let i = 0; i < CARDS.length; i++) {
+              let cardName = CARDS[i];
+              let owned = results[0][cardName];
+              if (owned) {
+                res.push(cardName + ".png");
+              }
+          }
+          callback(res);
+        });
+    }
+
+    ownedCardsHelper(res) {
+        console.log(res);
+    }
 
     exit() {
         this.connection.end(function(err) {
